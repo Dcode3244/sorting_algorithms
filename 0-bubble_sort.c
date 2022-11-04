12b127bbb2a82b324c2b40ce72f3476a423defb5
Dcode3244 <dagimourgie111@gmail.com>
2022-11-04 11:51:40 +0300

    bug fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,8 +8,8 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, x;
-	int temp, swap = 0;
+	size_t i, x;
+	int temp;
 
 	if (array == NULL || size < 2)
 		return;
@@ -24,10 +24,6 @@ void bubble_sort(int *array, size_t size)
 			array[i] = array[i + 1];
 			array[i + 1] = temp;
 			print_array(array, size);
-			swap = 1;
 		}
-		if (swap == 0)
-			break;
-		swap = 0;
 	}
 }
