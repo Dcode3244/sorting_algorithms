ce2a2a49c7992055f48b1d635eaf1cf2ab07a05a
Dcode3244 <dagimourgie111@gmail.com>
2022-11-05 10:43:41 +0300

    advanced task 6

---
+++ 102-counting_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * counting_sort - sorts an array of integers in ascending order
+ * using the Counting sort algorithm
+ * @array: the array to be sorted
+ * @size: size of the array
+ */
+void counting_sort(int *array, size_t size)
+{
+	int k = 0, idx;
+	size_t i;
+	int *count, *ordered;
+
+	for (i = 0; i < size; i++)
+		if (k < array[i])
+			k = array[i];
+
+	count = malloc(sizeof(int) * (k + 1));
+	if (count == NULL)
+		return;
+
+	for (i = 0; i <= (size_t)k; i++)
+		count[i] = 0;
+
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
+
+	for (i = 1; i <= (size_t)k; i++)
+		count[i] += count[i - 1];
+
+	print_array(count, k + 1);
+
+	ordered = malloc(sizeof(int) * size);
+	if (ordered == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		idx = count[array[i]]--;
+		ordered[idx - 1] = array[i];
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = ordered[i];
+
+	free(ordered);
+	free(count);
+}
